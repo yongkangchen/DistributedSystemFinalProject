ec5786d35029f62728a307df0a07a6a93befb04b
Yongkang Chen <lx1988cyk@gmail.com>
2022-11-28 21:08:34 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
+sudo apt install python3-pip
+sudo apt-get install python3-setuptools
+sudo apt-get install cmake
+sudo apt-get install python-dev-is-python3
+
+pip install setuptools
+
 pip install -r requirements.txt
